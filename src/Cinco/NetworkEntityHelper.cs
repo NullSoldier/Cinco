47ea8919aa5b979546779f3c0b9b90f582478b85
Jason Spafford <nullprogrammer@gmail.com>
2012-03-14 20:57:40 -0400

    Added a static helper to copy NetworkEntity fields

---
+++ src/Cinco/NetworkEntityHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cinco
+{
+	public static class NetworkEntityHelper
+	{
+		public static void CopyFrom (this NetworkEntity self, NetworkEntity networkEntity)
+		{
+			var deepCopy = networkEntity.DeepCopy ();
+
+			foreach (var kvp in deepCopy.Fields)
+				self.Fields[kvp.Key] = kvp.Value;
+		}
+	}
+}
